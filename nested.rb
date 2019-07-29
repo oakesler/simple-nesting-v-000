53f98ec682910522a8eefefb13fdcc742bf3f6e6
Olivia Kesler <oakesler@protonmail.ch>
2019-07-29 19:29:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -134,10 +134,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     
-  binding.pry 
   
-     programmer_hash[dennis_ritchie] = {:languages => "Assembly"}
+     programmer_hash[dennis_ritchie][languages] => {"Assembly"}
      
 
 end
